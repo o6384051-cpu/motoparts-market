7fa3df17c6484728d36645c26d5ce01a33e73879
o6384051-cpu <3.17570861e+08+o6384051-cpu@users.noreply.github.com>
2026-08-31 07:10:07 +0000

    Make NativeWind export CI safe

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,11 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+const isCi = process.env.CI === "true";
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
+  // NativeWind's disk cache is useful during local development but can
+  // produce an unreadable Metro SHA-1 entry on clean CI runners.
+  forceWriteFileSystem: !isCi,
 });
